aaec6c57702358631aaf3ac8c3603865a6c160bb
noble <noblebarch@gmail.com>
2024-10-02 12:40:08 +0300

    completed the tasks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,42 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, Container, Collapse } from "react-bootstrap";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <Navbar variant="dark" className="navbar " expand="lg">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto w-100 justify-content-between">
-            <div className="nav-left">
-              <Nav.Link className="text-white" href="#our-story">
-                Our Story
-              </Nav.Link>
-              <Nav.Link className="text-white" href="#watchlist">
-                Watchlist
-              </Nav.Link>
-            </div>
+  const [open, setOpen] = useState(false);
 
-            <div className="nav-right">
-              <Nav.Link className="text-white" href="#what-we-believe">
-                What We Believe
-              </Nav.Link>
-              <Nav.Link className="text-white" href="#our-future">
-                Our Future
-              </Nav.Link>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
+  return (
+    <Navbar variant="dark" className="navbar" expand="lg">
+      <Container fluid className="justify-content-between">
+        {/* Custom Hamburger Icon */}
+        <div
+          className={`custom-toggler ${open ? "active" : ""}`}
+          onClick={() => setOpen(!open)}
+        >
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <Collapse in={open}>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="w-100 justify-content-center">
+              <div className="nav-center">
+                <Nav.Link className="text-white" href="#our-story">
+                  Our Story
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#watchlist">
+                  Watchlist
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#what-we-believe">
+                  What We Believe
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#our-future">
+                  Our Future
+                </Nav.Link>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Collapse>
       </Container>
     </Navbar>
   );

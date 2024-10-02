36ad857209530b86e03bf84406dbb18c29507727
noble <noblebarch@gmail.com>
2024-10-02 17:01:49 +0300

    completed the tasks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,25 +3,29 @@ import { Navbar, Nav, Container, Collapse } from "react-bootstrap";
 import "./Header.css";
 
 const Header = () => {
-  const [open, setOpen] = useState(false); // State to control collapse
+  const [open, setOpen] = useState(false);
 
   return (
-    <Navbar variant="dark" className="navbar" expand="lg">
+    <Navbar variant="dark" className="navbar" expand="md">
+      {" "}
+      {/* Expand for md and larger */}
       <Container fluid className="justify-content-between">
-        {/* Custom Hamburger Icon */}
-        <div
-          className={`custom-toggler ${open ? "active" : ""} mt-4`}
-          onClick={() => setOpen(!open)}
-        >
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
+        {/* Hamburger Menu for small devices */}
+        <div className="d-md-none">
+          <div
+            className={`custom-toggler ${open ? "active" : ""} mt-4`}
+            onClick={() => setOpen(!open)}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
 
-        {/* The Collapse Component is controlled by the `open` state */}
-        <Collapse in={open}>
-          <div className="w-100"> {/* Added a wrapper div inside Collapse */}
-            <Navbar.Collapse id="basic-navbar-nav">
+          {/* The Collapse Component is controlled by the `open` state */}
+          <Collapse in={open}>
+            <div className="w-100">
+              {" "}
+              {/* Added a wrapper div inside Collapse */}
               <Nav className="w-100 justify-content-center">
                 <div className="nav-center">
                   <Nav.Link className="text-white" href="#our-story">
@@ -38,9 +42,33 @@ const Header = () => {
                   </Nav.Link>
                 </div>
               </Nav>
-            </Navbar.Collapse>
-          </div>
-        </Collapse>
+            </div>
+          </Collapse>
+        </div>
+
+        <Navbar.Collapse id="basic-navbar-nav" className="d-none d-md-flex">
+          <Nav className=" w-100 justify-content-center">
+            <div className="normal-nav">
+              <Nav className="">
+                <Nav.Link className="text-white mx-4" href="#our-story">
+                  Our Story
+                </Nav.Link>
+                <Nav.Link className="text-white mx-4" href="#watchlist">
+                  Watchlist
+                </Nav.Link>
+              </Nav>
+              <Nav>
+                {" "}
+                <Nav.Link className="text-white mx-4" href="#what-we-believe">
+                  What We Believe
+                </Nav.Link>
+                <Nav.Link className="text-white mx-4 " href="#our-future">
+                  Our Future
+                </Nav.Link>
+              </Nav>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );

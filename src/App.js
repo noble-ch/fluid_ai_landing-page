962ac93bfa051dafc1f413c3246edba6dbf1597d
noble <noblebarch@gmail.com>
2024-10-02 12:33:06 +0300

    completed the tasks

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import ImageCarousel from "./components/Carousel/ImageCarousel.jsx";
 import CuriosityBenefits from "./components/CuriosityBenefits/CuriosityBenefits.js";
 import WaitlistForm from "./components/WaitlistForm/WaitlistForm.js";
 import Footer from "./components/Footer/Footer.js";
-import Preloader from "./components/Preloader/Preloader"; // Import Preloader
+import Preloader from "./components/Preloader/Preloader"; 
 import "./App.css";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Set the spinner to hide after 5 seconds
     const timer = setTimeout(() => {
       setIsLoading(false); 
     }, 3000);
